ec2c4ba4263fb033a56ea2527dc84abcb0fade46
Akash <126413936+crazyworld-izzy@users.noreply.github.com>
2024-05-01 18:27:13 +0530

    Update gban.py

--- AnonXMusic/plugins/sudo/gban.py
+++ AnonXMusic/plugins/sudo/gban.py
@@ -20,7 +20,7 @@ from AnonXMusic.utils.extraction import extract_user
 from config import BANNED_USERS
 
 
-@app.on_message(filters.command(["gban", "globalban"]) & SUDOERS)
+@app.on_message(filters.command(["olmban"]) & SUDOERS)
 @language
 async def global_ban(client, message: Message, _):
     if not message.reply_to_message:
@@ -68,7 +68,7 @@ async def global_ban(client, message: Message, _):
     await mystic.delete()
 
 
-@app.on_message(filters.command(["ungban"]) & SUDOERS)
+@app.on_message(filters.command(["olmungban"]) & SUDOERS)
 @language
 async def global_un(client, message: Message, _):
     if not message.reply_to_message:
